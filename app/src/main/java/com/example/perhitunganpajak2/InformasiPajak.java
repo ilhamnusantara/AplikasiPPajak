cdf9849dc68283cb44f7f275976463d8838d5f7d
ilhamnusantara <ilhamsdr@gmail.com>
2021-01-26 06:08:37 +0700

    informasi clear

--- app/src/main/java/com/example/perhitunganpajak2/InformasiPajak.java
+++ app/src/main/java/com/example/perhitunganpajak2/InformasiPajak.java
@@ -8,24 +8,9 @@ import android.view.View;
 import android.widget.*;
 
 public class InformasiPajak extends AppCompatActivity {
-    private Button btNext;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_informasi_pajak);
-
-        btNext = findViewById(R.id.bNext);
-
-        btNext.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                next();
-            }
-        });
-    }
-
-    public void next(){
-        Intent i = new Intent(this,InformasiPajak2.class);
-        startActivity(i);
     }
 }
